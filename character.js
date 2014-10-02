fc6ee6197c03531bca1ce79569359ae651b2c015
rwongoberlin <rwong@oberlin.edu>
2014-10-02 10:25:14 -0400

    Create character.js

---
+++ character.js
@@ -0,0 +1,18 @@
+var x : float;
+var y : float;
+var comingFrom : float;
+var goingTo : float;
+
+var currentTile : tile; 
+var nextTile : tile; 
+var clock : float;
+var tempTime : float;
+var speed : float;
+
+var tiles : Array;
+
+function init(row : float, column : float, r : float, Tile : tile, tileList : Array, typeL : int) {
+	var modelObject = GameObject.CreatePrimitive(PrimitiveType.Quad);	// Create a quad object for holding the marble texture.
+	model = modelObject.AddComponent("marbleModel");					// Add a marbleModel script to control visuals of the marble.
+	model.init(this, row, column, r, Tile, tileList, typeL);													// Initialize the model.   
+}
