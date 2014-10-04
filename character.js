b66e5b6638dc85b594c9c0c9b3cee47518155da4
amebaman <timxavhen@gmail.com>
2014-10-04 14:53:14 -0400

    Update character.js

--- character.js
+++ character.js
@@ -10,9 +10,11 @@ var tempTime : float;
 var speed : float;
 
 var tiles : Array;
+var targets : int;
 
 function init(row : float, column : float, r : float, Tile : tile, tileList : Array, typeL : int) {
 	var modelObject = GameObject.CreatePrimitive(PrimitiveType.Quad);	// Create a quad object for holding the marble texture.
-	model = modelObject.AddComponent("marbleModel");					// Add a marbleModel script to control visuals of the marble.
-	model.init(this, row, column, r, Tile, tileList, typeL);													// Initialize the model.   
+	model = modelObject.AddComponent("characterModel");					// Add a marbleModel script to control visuals of the marble.
+	model.init(this, row, column, r, Tile, tileList, typeL);													// Initialize the model.
+	targets = 0;
 }
