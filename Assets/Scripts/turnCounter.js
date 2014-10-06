c858e44c73eed44a0c6e240eed4f516518a46c35
Devin Frenze <frenze.devin@gmail.com>
2014-10-06 12:41:13 -0400

    Reset, Turn Counter, and Text-Based Map Builder
    First commit working on Reset, Turn Counter, and Text-Based Map
    Builder. All are implemented properly but do not line up with rest of
    game because the other parts either haven’t been made yet or need to be
    adapted to cooperate.

---
+++ Assets/Scripts/turnCounter.js
@@ -0,0 +1,34 @@
+﻿/*
+class created by Devin
+*/
+var owner : gameManager;
+var turns : float;
+var counterText : TextMesh;
+
+function init(o : gameManager) {
+	owner = o;
+	turns = 0;
+	
+	transform.localScale = Vector3(0.1, 0.1, 0.1);
+	counterText = gameObject.AddComponent(TextMesh).GetComponent(TextMesh);
+	counterText.text = ""+Mathf.Floor(turns);
+	counterText.color = Color(1,1,1, 1f);
+	counterText.offsetZ = -1;
+	counterText.fontSize = 64;
+	var courier : Font =  Resources.Load("Fonts/CourierNew") as Font;
+	counterText.font = courier;
+	
+	//gameObject.renderer.material = Resources.Load("Scripts/textMat");
+	gameObject.renderer.material = courier.material;
+	//gameObject.renderer.material.color = Color(0,0,0, 0f);
+}
+
+function halfMove() {
+	turns = turns + 0.5;
+	counterText.text = ""+Mathf.Floor(turns);
+}
+
+function reset() {
+	turns = 0;
+	counterText.text = ""+Mathf.Floor(turns);
+}
