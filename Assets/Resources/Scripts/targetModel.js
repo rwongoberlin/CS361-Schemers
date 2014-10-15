8dabee031a6c7ef91e0c96120ee440dfcf9589d1
Robin <rwong@oberlin.edu>
2014-10-14 22:25:39 -0400

    hopefully old stuff

---
+++ Assets/Resources/Scripts/targetModel.js
@@ -0,0 +1,17 @@
+﻿var owner : target;
+var type : int;
+var count : int;
+
+function init(o : target, typeS : int, countS : int) {
+	owner = o;
+	type = typeS;
+	count = countS;
+	
+	transform.parent = owner.transform;				
+	transform.localPosition = Vector3(0,0, -0.001);	
+	name = "Target Model";							
+	
+	renderer.material.mainTexture = Resources.Load("Textures/gem" + type, Texture2D);	
+	renderer.material.color = Color(1,1,1);										
+	renderer.material.shader = Shader.Find ("Transparent/Diffuse");	
+}
