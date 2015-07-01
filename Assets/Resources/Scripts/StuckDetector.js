7c510bea30eaff91924f326437a791dee2446d54
Tim Hennigan <Tim@ip-83-95.wireless.oberlin.edu>
2015-07-01 15:33:03 -0400

    still working on StuckDetector

--- Assets/Resources/Scripts/StuckDetector.js
+++ Assets/Resources/Scripts/StuckDetector.js
@@ -7,7 +7,7 @@ var width : int;
 function init (tiles : Array, h : int, w : int){
 	height = h;
 	width = w;
-	numNodes = height*width*16 + 1;
+	numNodes = width*height*width*height*16+1;
 	graph = new Array(numNodes);
 	for (int i = 0; i < numNodes) {
 		graph[i] = new Array(numNodes);
@@ -15,9 +15,50 @@ function init (tiles : Array, h : int, w : int){
 	fillGraph();
 }
 
-function getGraphEntry(x : int, y : int, greenTargets : int, purpleTargets : int) {
-	return graph[(x * width * 16) + (y * 16) + (greenTargets * 4) + purpleTargets];
+function getLinearFromCoords(x : int, y : int) {
+	return x*height + y;
+}
+
+function getXFromLinear(num : int) {
+	return (int)(num / height);
+}
+
+function getYFromLinear(num : int) {
+	return num % height;
+}
+
+function getGraphEntry(greenPosX : int, GreenPosY : int, PurplePosX : int, PurplePosY : int, greenTargets : int, purpleTargets : int) {
+	var GreenPos = getLinearFromCoords(GreenPosX, GreenPosY);
+	var PurplePos = getLinearFromCoords(PurplePosX, PurplePosY);
+	return GreenPos*width*height*16 + PurplePos*16 + greenTargets*4 + PurpleTargets;
+}
+
+function getGreenCoords(num : int) {
+	return num / (width * height * 16);
+}
+
+function getPurpleCoords(num : int) {
+	return (num % (width * height * 16)) / 16;
 }
 
 function fillGraph() {
+	var gx : int;
+	var gy : int;
+	var px : int;
+	var py : int;
+	for (int i = 0; i < numNodes - 1) {
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
